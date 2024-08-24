df241f02c79dc972ece64d8755846db5f8c20b8e
Dipe98 <diegoperezfi98@gmail.com>
2024-08-23 21:55:17 -0400

    Evaluacion modulo 2

---
+++ modulo 2/Evaluacion modulo 2/assets/js/script.js
@@ -0,0 +1,18 @@
+// alert formulario contacto
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        alert('Gracias por contactarnos. Nos pondremos en contacto contigo pronto.');
+    });
+
+
+
+// Al dar click en boton se abre otra pagina web 
+document.getElementById("btnPerfilLinkedin").onclick = function () {
+     window.location.href = "https://www.linkedin.com/in/diegoperezfi/";
+        };
+document.getElementById("btnCertificacion").onclick = function () {
+     window.location.href = "https://www.credly.com/badges/fe29f972-6894-4fe0-877b-407ccc897c92/linked_in_profile";
+        };
+document.getElementById("btnPortafolioGitHub").onclick = function () {
+    window.location.href = "https://github.com/Dipe98/Java0082";
+        };
+   
